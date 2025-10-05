561680acf44a027d2ea56af04ef255d0e3814f19
LewisUwase <122404254+LewisUwase@users.noreply.github.com>
2025-10-05 15:59:24 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,52 +1,20 @@
-import java.util.*;
+package storefront;
 
 public class Item {
 
-	private Collection<OrderLineItem> product;
-	private Collection<Store> store;
-	private Collection<Price> prices;
-	private Collection<UPC> productUpc;
-	private String description;
-	private STRING itemNumber;
-	private UPC upcs;
-	private TaxCategory taxCategory;
-	private OrderLineItem orderLine;
+    private String name;
+    private double price;
 
-	public void getPriceForQuantity() {
-		// TODO - implement Item.getPriceForQuantity
-		throw new UnsupportedOperationException();
-	}
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 
-	/**
-	 * 
-	 * @param price
-	 */
-	public void addPrice(Price price) {
-		// TODO - implement Item.addPrice
-		throw new UnsupportedOperationException();
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Item() {
-		// TODO - implement Item.Item
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param price
-	 */
-	public void removePrice(Price price) {
-		// TODO - implement Item.removePrice
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param date
-	 */
-	public void getTaxRateForDate(LocalDate date) {
-		// TODO - implement Item.getTaxRateForDate
-		throw new UnsupportedOperationException();
-	}
-
+    public double getPrice() {
+        return price;
+    }
 }
